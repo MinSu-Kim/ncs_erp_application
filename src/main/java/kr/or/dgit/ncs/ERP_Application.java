8c7352faaa2a74d62cc60719b5905d54700b4e26
mskim <net94@naver.com>
2017-04-21 13:58:06 +0900

    메인 화면 수정

--- src/main/java/kr/or/dgit/ncs/ERP_Application.java
+++ src/main/java/kr/or/dgit/ncs/ERP_Application.java
@@ -10,11 +10,18 @@ import javax.swing.border.EmptyBorder;
 import kr.or.dgit.ncs.view.ViewDepartment;
 import kr.or.dgit.ncs.view.ViewEmployee;
 import kr.or.dgit.ncs.view.ViewTitle;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 @SuppressWarnings("serial")
-public class ERP_Application extends JFrame {
+public class ERP_Application extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
+	private JButton btnEmp;
+	private JButton btnDept;
+	private JButton btnTitle;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -38,12 +45,42 @@ public class ERP_Application extends JFrame {
 	}
 
 	public ERP_Application() {
+		setTitle("대구아이티ERP");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 486, 118);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 0, 10, 0));
+		
+		btnEmp = new JButton("사원관리");
+		btnEmp.addActionListener(this);
+		contentPane.add(btnEmp);
+		
+		btnDept = new JButton("부서관리");
+		btnDept.addActionListener(this);
+		contentPane.add(btnDept);
+		
+		btnTitle = new JButton("직책관리");
+		btnTitle.addActionListener(this);
+		contentPane.add(btnTitle);
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnTitle) {
+			actionPerformedBtnTitle(e);
+		}
+		if (e.getSource() == btnDept) {
+			actionPerformedBtnDept(e);
+		}
+		if (e.getSource() == btnEmp) {
+			actionPerformedBtnEmp(e);
+		}
+	}
+	protected void actionPerformedBtnEmp(ActionEvent e) {
+	}
+	protected void actionPerformedBtnDept(ActionEvent e) {
+	}
+	protected void actionPerformedBtnTitle(ActionEvent e) {
+	}
 }
